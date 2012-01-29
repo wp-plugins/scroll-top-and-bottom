82896d56579eac8e85f2dc2e95c9ad5f1a3e35aa
shariarbd <shariarbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-29 17:11:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scroll-top-and-bottom/trunk@496940 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Scroll Top and Bottom
-Plugin URI: http://shariarbd.com/plugin/scroll-top-and-bottom/
+Plugin URI: http://shariarbd.com/plugins/scroll-top-and-bottom/
 Description: Scroll Top and Bottom is created to scroll to top and bottom. User can go to top and bottom of the site using this tool easily .Plugin is created by <cite><a href="http://shariarbd.com/" title="Md. Sahriar">Md. Shariar</a>.</cite>
 Version: 1.0
 Author: Md. Shariar
