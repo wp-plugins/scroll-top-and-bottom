979547efa1ca92496620fae33e06a48f4c91de16
shariarbd <shariarbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-19 11:41:47 +0000

    updates
    git-svn-id: https://plugins.svn.wordpress.org/scroll-top-and-bottom/tags/2.5@860875 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Plugin Name ===
+
+    Contributors: shariarbd
+    Plugin Name: Scroll Top And Botton
+    Plugin URI: http://shariarbd.com/plugins/scroll-top-and-bottom/
+    Tags: Scroll, Top And Botton, scroll up and down, Navigate, jQuery Scrolling 
+    Author URI: http://shariarbd.com/
+    Author: Md. Shariar
+    Donate link: http://shariarbd.com/
+    Requires at least: WordPress 3.0 
+    Tested up to: 3.8.1 
+    Scroll Top and Bottom is created to scroll to top and bottom of the site. 12 Different Icon are available now. 
+
+
+== Description ==
+   Scroll Top and Bottom is created to scroll to top and bottom of the site. User can go to top and bottom of the site using this tool easily. Will response on Mobile Device as the button will not display if width is below 500px. Now, 12 different icon has been added and you can choose them from settings. This plugin is created with jQuery and <a href="http://shariarbd.com/" target="_blank">Demo is here.</a> Plugin is created by <cite><a href="http://shariarbd.com/" title="Md. Sahriar">Md. Shariar</a>.</cite>
+
+
+== Installation ==
+
+1. Upload `scroll-top-and-bottom` folder to the `/wp-content/plugins/` directory
+(this can be done automatically via the WordPress 2.8 Plugin Browser/Installer interface or later)
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Make sure that the wordpress function wp_head()  and wp_footer() are included on your theme.
+
+
+
+
+== Screenshots ==
+
+1. Settings Page of the plugis.
---
+++ tandb.php
@@ -0,0 +1,41 @@
+		<?php $wp_STAB_icon_Select = get_option('STAB_icon_Select');
+			 if($wp_STAB_icon_Select != true){
+			 $wp_STAB_icon_Select =2;
+			 }
+		
+		 ?>
+        <div style="display:none;" class="scroll-to-top" id="scroll-to-top">
+         <?php echo '<img alt="&uarr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_u.ico' , dirname(__FILE__) ). '" > '; ?>
+        </div>
+		<div style="display:block;" class="scroll-to-bottom" id="scroll-to-bottom">
+         <?php echo '<img alt="&darr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_d.ico' , dirname(__FILE__) ). '" > '; ?>
+        </div>
+		
+		<script>
+			$(document).ready(function() {
+
+				$(window).scroll(function() {
+				      if ($(this).scrollTop() > 200) {
+				         $('#scroll-to-top').fadeIn('slow').css('right','66px');
+				      } else {
+				          $('#scroll-to-top').fadeOut('slow');
+				      }
+
+					if ($(this).scrollTop() > $(document).height() - screen.height) {
+				         $('#scroll-to-bottom').fadeOut('slow');
+				      } else {
+				         $('#scroll-to-bottom').fadeIn('slow');
+				      }
+
+				 }); 
+			
+				   $('.scroll-to-top ').click(function(){
+				      $("html,documentElement").animate({ scrollTop: 0 }, 1000);
+				      return false;
+				   });
+				   $('.scroll-to-bottom').click(function(){
+				      $('html,documentElement').animate({scrollTop: $(document).height()}, 1000);
+				      return false;
+				   });
+            });
+ 			</script>
