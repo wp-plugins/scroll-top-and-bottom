4bbd34b35508d4a043c4ea2e6ab0b1b1441621bb
shariarbd <shariarbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-08 18:47:05 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/scroll-top-and-bottom/tags/2.6@871932 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+
+    Contributors: shariarbd
+    Plugin Name: Scroll Top And Botton
+    Plugin URI: http://shariarbd.com/plugins/scroll-top-and-bottom/
+    Tags: Scroll, Top And Botton, scroll up and down, Navigate, jQuery Scrolling 
+    Author URI: http://shariarbd.com/
+    Author: Md. Shariar
+    Donate link: http://shariarbd.com/
+    Requires at least: WordPress 3.0 
+    Tested up to: 3.8.1 
+    Scroll Top and Bottom is created to scroll to top and bottom of the site. 12 Different Icon are available now and also you can control scrolling pseed! 
+
+
+== Description ==
+   Scroll Top and Bottom is created to scroll to top and bottom of the site. User can go to top and bottom of the site using this tool easily. Now you can control the scrolling speed! 12 different icon has been added and you can choose them from settings. This plugin is created with jQuery and <a href="http://shariarbd.com/" target="_blank">Demo is here.</a> Plugin is created by <cite><a href="http://shariarbd.com/" title="Md. Sahriar">Md. Shariar</a>.</cite>
+
+
+== Installation ==
+
+1. Upload `scroll-top-and-bottom` folder to the `/wp-content/plugins/` directory
+(this can be done automatically via the WordPress 2.8 Plugin Browser/Installer interface or later)
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Make sure that the wordpress function wp_head()  and wp_footer() are included on your theme.
+
+
+== Frequently asked questions ==
+
+= Does it support Internet Explorer ? =
+
+Yes, but required at least IE9 or later.
+
+= Can I change Scroll Speed? =
+
+Yes, see setting of the Plugin. But please be sure that the entered value is just a number and nothing. Example 1000, 1500 etc here 1000=1s.
+
+= Can I change buttons? =
+
+Normally No, But you can choose from 12 different buttons from settings.
+
+
+== Screenshots ==
+
+1. Settings Page of the plugis.
---
+++ stab.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Plugin Name: Scroll Top and Bottom
+Plugin URI: http://shariarbd.com/plugins/scroll-top-and-bottom/
+Description: Scroll Top and Bottom is created to scroll to top and bottom of the site. User can go to top and bottom of the site using this tool easily. Now you can control the scrolling speed! 12 different icon has been added and you can choose them from settings. This plugin is created with jQuery and <a href="http://shariarbd.com/" target="_blank">Demo is here.</a> Plugin is created by <cite><a href="http://shariarbd.com/" title="Md. Sahriar">Md. Shariar</a>.</cite>
+Version: 2.6
+Author: Md. Shariar
+Author URI: http://shariarbd.com/ 
+*/
+
+
+// Admin Portion
+ function STAB_OPTION()
+{
+    if($_POST['STAB_UPDATE']){
+		update_option('STAB_icon_Select',$_POST['STAB_icon_Select']);
+		update_option('STAB_jquery',$_POST['STAB_jquery']);
+    update_option('STAB_scroll_speed',$_POST['STAB_scroll_speed']);
+        echo '<h3>Plugin has been updated.</h3>';
+	}
+	$wp_STAB_icon_Select = get_option('STAB_icon_Select');
+	$wp_STAB_jQuery_deactive = get_option('STAB_jQuery_deactive');
+  $wp_STAB_scroll_speed = get_option('STAB_scroll_speed');
+	?>
+
+<div class="wrap">
+  <form method="post" id="STAB_OPTION">
+  <h2>Scroll Top and Bottom Option Page</h2>
+
+  <h3>Choose Icon</h3>
+
+<?php
+$icondr="scroll-top-and-bottom/icon/";
+?>
+	<style type="text/css">
+	.icon_choose input{
+		margin-bottom:10px}
+    </style>	 
+    <table class="icon_choose" width="350" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td> 
+          <input type="radio" id="STAB_icon_Select_1" name="STAB_icon_Select" value="1" <?php if($wp_STAB_icon_Select == 1) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_1">
+  			     <?php echo '<img src="' .plugins_url( ''.$icondr.'1_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'1_d.ico' , dirname(__FILE__) ). '" > '; ?>
+  			  </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_2" name="STAB_icon_Select" value="2" <?php if($wp_STAB_icon_Select == 2) { echo('checked="checked"'); } if($wp_STAB_icon_Select != true) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_2">
+  			     <?php echo '<img src="' .plugins_url( ''.$icondr.'2_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'2_d.ico' , dirname(__FILE__) ). '" > '; ?>
+  			  </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_3" name="STAB_icon_Select" value="3" <?php if($wp_STAB_icon_Select == 3) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_3">
+  			     <?php echo '<img src="' .plugins_url( ''.$icondr.'3_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'3_d.ico' , dirname(__FILE__) ). '" > '; ?>
+  			  </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_4" name="STAB_icon_Select" value="4" <?php if($wp_STAB_icon_Select == 4) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_4">
+  			     <?php echo '<img src="' .plugins_url( ''.$icondr.'4_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'4_d.ico' , dirname(__FILE__) ). '" > '; ?>
+  			  </label>
+        </td>
+
+        <td>
+          <input type="radio" id="STAB_icon_Select_5" name="STAB_icon_Select" value="5" <?php if($wp_STAB_icon_Select == 5) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_5">
+  			     <?php echo '<img src="' .plugins_url( ''.$icondr.'5_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'5_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			    </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_6" name="STAB_icon_Select" value="6" <?php if($wp_STAB_icon_Select == 6) { echo('checked="checked"'); }  ?> />
+          <label for="STAB_icon_Select_6">
+			       <?php echo '<img src="' .plugins_url( ''.$icondr.'6_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'6_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			    </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_7" name="STAB_icon_Select" value="7" <?php if($wp_STAB_icon_Select == 7) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_7">
+			       <?php echo '<img src="' .plugins_url( ''.$icondr.'7_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'7_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			    </label>
+          <br/>
+          <input type="radio" id="STAB_icon_Select_8" name="STAB_icon_Select" value="8" <?php if($wp_STAB_icon_Select == 8) { echo('checked="checked"'); } ?> />
+          <label for="STAB_icon_Select_8">
+			       <?php echo '<img src="' .plugins_url( ''.$icondr.'8_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'8_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			    </label>
+        </td>
+
+        <td>
+            <input type="radio" id="STAB_icon_Select_9" name="STAB_icon_Select" value="9" <?php if($wp_STAB_icon_Select == 9) { echo('checked="checked"'); } ?> />
+            <label for="STAB_icon_Select_9">
+			         <?php echo '<img src="' .plugins_url( ''.$icondr.'9_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'9_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			      </label>
+            <br/>
+            <input type="radio" id="STAB_icon_Select_10" name="STAB_icon_Select" value="10" <?php if($wp_STAB_icon_Select == 10) { echo('checked="checked"'); }  ?> />
+            <label for="STAB_icon_Select_10">
+			         <?php echo '<img src="' .plugins_url( ''.$icondr.'10_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'10_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			       </label>
+             <br/>
+            <input type="radio" id="STAB_icon_Select_11" name="STAB_icon_Select" value="11" <?php if($wp_STAB_icon_Select == 11) { echo('checked="checked"'); } ?> />
+            <label for="STAB_icon_Select_11">
+			         <?php echo '<img src="' .plugins_url( ''.$icondr.'11_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'11_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			       </label>
+            <br/>
+            <input type="radio" id="STAB_icon_Select_12" name="STAB_icon_Select" value="12" <?php if($wp_STAB_icon_Select == 12) { echo('checked="checked"'); } ?> />
+            <label for="STAB_icon_Select_12">
+			         <?php echo '<img src="' .plugins_url( ''.$icondr.'12_u.ico' , dirname(__FILE__) ). '" > '; echo '<img src="' .plugins_url( ''.$icondr.'12_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			      </label>
+        </td>
+      </tr>
+    </table>
+
+            <label for="STAB_scroll_speed"><h3>Scroll Speed</h3></label>
+            <input type="text" name="STAB_scroll_speed" id="STAB_scroll_speed" value="<?php if($wp_STAB_scroll_speed=="") echo "1000"; else echo "$wp_STAB_scroll_speed";?>" > Note: 1000=1s and this field must be a number.
+
+            <h3>jQuery Enable – Disable</h3>
+            <h4>
+              <?php $wp_STAB_jquery = get_option('STAB_jquery'); ?>
+              <input name="STAB_jquery" type="radio" id="STAB_jquery_yes" value="yes" <?php if($wp_STAB_jquery != "no") { echo('checked="checked"'); } ?> /><label for="STAB_jquery_yes"> jQuery Enabled</label>
+              <br>
+              <input name="STAB_jquery" type="radio" id="STAB_jquery_no" value="no" <?php if($wp_STAB_jquery == "no") { echo('checked="checked"'); } ?> /><label for="STAB_jquery_no"> jQuery Disabled</label>
+            </h4>
+            <span style="color:green; font-size:14px; text-transform:uppercase;">If your site already use jQuery from before, it may not work. In that case, Disable jQuery here and it may work! </span>
+            <div style="float:right; color:#09F; font-size:14px; text-transform:uppercase;">
+                <strong>If you love this plugin, <a href="http://wordpress.org/extend/plugins/scroll-top-and-bottom/" target="_blank">Please Rate as five star.</a></strong>
+            </div>
+        <div style="overflow:hidden; margin-top:20px;">
+          <input type="submit" name="STAB_UPDATE" value="Update" />
+        </div>
+  </form><!-- /#STAB_OPTION -->
+</div> <!-- /.wrap -->
+<?php
+}
+
+
+
+
+
+function STAB_ADMIN()
+{
+	if (function_exists('add_options_page')) {
+		add_options_page('Scroll Top-Bottom', 'Scroll Top-Bottom', 9, basename(__FILE__),'STAB_OPTION');
+	}
+}
+
+function STAB_actions( $links, $file ) {
+		if( $file == 'scroll-top-and-bottom/stab.php' && function_exists( "admin_url" ) ) {
+			$settings_link = '<a href="' . admin_url( 'options-general.php?page=stab.php' ) . '">' .'Settings' . '</a>';
+			array_unshift( $links, $settings_link ); // before other links
+		}
+		return $links;
+}
+
+
+function scroll_tandbhead() 
+{
+$wp_STAB_jquery = get_option('STAB_jquery');
+if($wp_STAB_jquery=="no")
+{	
+echo '<link rel="stylesheet" type="text/css" href="'.plugins_url( 'scroll-top-and-bottom/css/style.css' , dirname(__FILE__) ). '" />'; 
+}
+else{
+echo '<script type="text/javascript" src="' .plugins_url( 'scroll-top-and-bottom/js/jquery-2.1.0.min.js' , dirname(__FILE__) ). '" ></script>';
+echo '<link rel="stylesheet" type="text/css" href="'.plugins_url( 'scroll-top-and-bottom/css/style.css' , dirname(__FILE__) ). '" />'; 
+}
+}
+
+function scroll_tandb() 
+{
+include('tandb.php');
+}
+
+add_action('admin_menu','STAB_ADMIN',1);
+add_filter('plugin_action_links', 'STAB_actions', 10, 2 );
+add_action('wp_head', 'scroll_tandbhead');
+add_action('wp_footer', 'scroll_tandb');
+?>
