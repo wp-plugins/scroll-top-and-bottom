24e6cdafad74285092e3e89a1ec5e6b67c7a927b
shariarbd <shariarbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-29 19:09:58 +0000

    Manual jQuery Loading removed and now it's loaded the default jquery library of WP. Also chanced the necessary js accordingly
    git-svn-id: https://plugins.svn.wordpress.org/scroll-top-and-bottom/tags/2.8@923268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tandb.php
@@ -0,0 +1,55 @@
+		<?php $wp_STAB_icon_Select = get_option('STAB_icon_Select');
+			  $wp_STAB_scroll_speed = get_option('STAB_scroll_speed');
+			 if($wp_STAB_icon_Select != true){
+			 $wp_STAB_icon_Select =2;
+			 }
+			 if($wp_STAB_icon_Select=="13")
+			 {
+			 	$stab_up_btn = get_option('stab_up_btn');
+    			$stab_down_btn = get_option('stab_down_btn');
+
+    			?>
+		        <div style="display:none;" class="scroll-to-top" id="scroll-to-top">
+		         <?php echo '<img alt="&uarr;" width="32" height="32" src="' .$stab_up_btn. '" > '; ?>
+		        </div>
+				<div style="display:block;" class="scroll-to-bottom" id="scroll-to-bottom">
+		         <?php echo '<img alt="&darr;" width="32" height="32" src="' .$stab_down_btn. '" > '; ?>
+		        </div>
+				<?php
+				}else{
+		 ?>
+        <div style="display:none;" class="scroll-to-top" id="scroll-to-top">
+         <?php echo '<img alt="&uarr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_u.ico' , dirname(__FILE__) ). '" > '; ?>
+        </div>
+		<div style="display:block;" class="scroll-to-bottom" id="scroll-to-bottom">
+         <?php echo '<img alt="&darr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_d.ico' , dirname(__FILE__) ). '" > '; ?>
+        </div>
+		<?php } ?>
+		<script>
+			$(document).ready(function() {
+
+				$(window).scroll(function() {
+				      if ($(this).scrollTop() > 200) {
+				         $('#scroll-to-top').fadeIn('slow');
+				      } else {
+				          $('#scroll-to-top').fadeOut('slow');
+				      }
+
+					if ($(this).scrollTop() > $(document).height() - screen.height) {
+				         $('#scroll-to-bottom').fadeOut('slow');
+				      } else {
+				         $('#scroll-to-bottom').fadeIn('slow');
+				      }
+
+				 }); 
+			
+				   $('.scroll-to-top').click(function(){
+				      $("html,body").animate({ scrollTop: 0 }, <?php if($wp_STAB_scroll_speed=="") echo "1000"; else echo "$wp_STAB_scroll_speed";?>);
+				      return false;
+				   });
+				   $('.scroll-to-bottom').click(function(){
+				      $('html,body').animate({scrollTop: $(document).height()}, <?php if($wp_STAB_scroll_speed=="") echo "1000"; else echo "$wp_STAB_scroll_speed";?>);
+				      return false;
+				   });
+            });
+ 			</script>
