6f8496783d2bb1923dc8984d95038f1bae4e226b
shariarbd <shariarbd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 14:27:34 +0000

    Version Update to 2.8.5 Added some features like "Hide on Posts", "Show only on Home page", "which button will display" etc ...
    git-svn-id: https://plugins.svn.wordpress.org/scroll-top-and-bottom/tags/2.8.3@1108116 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tandb.php
@@ -0,0 +1,45 @@
+		<div class="scroll-btn-container">
+			<?php $wp_STAB_icon_Select = get_option('STAB_icon_Select');
+			$wp_STAB_scroll_speed = get_option('STAB_scroll_speed');
+			if($wp_STAB_icon_Select != true){
+				$wp_STAB_icon_Select =2;
+			}
+			if($wp_STAB_icon_Select=="13")
+			{
+				$stab_up_btn = get_option('stab_up_btn');
+				$stab_down_btn = get_option('stab_down_btn');
+
+				?>
+				
+					<div style="display:none;" class="scroll-to-top" id="scroll-to-top">
+						<?php echo '<img alt="&uarr;" width="32" height="32" src="' .$stab_up_btn. '" > '; ?>
+					</div>
+					<div style="display:block;" class="scroll-to-bottom" id="scroll-to-bottom">
+						<?php echo '<img alt="&darr;" width="32" height="32" src="' .$stab_down_btn. '" > '; ?>
+					</div>
+						
+			<?php
+			}else{
+			?>
+			<div style="display:none;" class="scroll-to-top" id="scroll-to-top">
+				<?php echo '<img alt="&uarr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_u.ico' , dirname(__FILE__) ). '" > '; ?>
+			</div>
+			<div style="display:block;" class="scroll-to-bottom" id="scroll-to-bottom">
+				<?php echo '<img alt="&darr;" width="32" height="32" src="' .plugins_url( 'scroll-top-and-bottom/icon/'.$wp_STAB_icon_Select.'_d.ico' , dirname(__FILE__) ). '" > '; ?>
+			</div>
+			<?php } ?>
+			</div>	
+
+			<script>
+				jQuery(document).ready(function() {
+
+					jQuery('.scroll-to-top').click(function(){
+						jQuery("html,body").animate({ scrollTop: 0 }, <?php if($wp_STAB_scroll_speed=="") echo "1000"; else echo "$wp_STAB_scroll_speed";?>);
+						return false;
+					});
+					jQuery('.scroll-to-bottom').click(function(){
+						jQuery('html,body').animate({scrollTop: jQuery(document).height()}, <?php if($wp_STAB_scroll_speed=="") echo "1000"; else echo "$wp_STAB_scroll_speed";?>);
+						return false;
+					});
+				});
+			</script>
